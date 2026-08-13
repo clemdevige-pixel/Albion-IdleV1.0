f683101f15f87fba37e03309ef8a07e2e0c76fa8
Kowzil <clem.devige@gmail.com>
2026-08-14 00:50:23 +0200

    refactor: anchor status effects outside top HUD flow

--- apps/client/src/hud/HudRoot.tsx
+++ apps/client/src/hud/HudRoot.tsx
@@ -1,16 +1,14 @@
 import { CombatStateBar } from "./CombatStateBar";
 import { ActiveEffectsDisplay } from "./ActiveEffectsDisplay";
 
-/**
- * HUD overlay positioned on top of the Phaser canvas.
- */
+/** HUD overlay positioned on top of the Phaser canvas. */
 export function HudRoot(): JSX.Element {
   return (
     <div className="hud-root">
       <div className="hud-root__top">
         <CombatStateBar />
-        <ActiveEffectsDisplay />
       </div>
+      <ActiveEffectsDisplay />
     </div>
   );
 }
