6305ce857004c980218a1b810389223a76af8f5a
Kowzil <clem.devige@gmail.com>
2026-08-14 18:15:36 +0200

    feat(island-ui): switch central world to island view

--- apps/client/src/ui/shell/AppShell.tsx
+++ apps/client/src/ui/shell/AppShell.tsx
@@ -1,15 +1,24 @@
 import { RightPanelHost } from "../modules/RightPanelHost";
+import { useNavigation } from "../navigation";
+import { UI_MODULE_IDS } from "../navigation/moduleIds";
+import { IslandSelectionProvider } from "../island/IslandSelectionContext";
+import { IslandWorldRegion } from "../island/IslandWorldRegion";
 import { BottomBarRegion } from "./BottomBarRegion";
 import { GameWorldRegion } from "./GameWorldRegion";
 import { HeaderRegion } from "./HeaderRegion";
 
 export function AppShell(): JSX.Element {
+  const { activeModule } = useNavigation();
+  const isIsland = activeModule === UI_MODULE_IDS.island;
+
   return (
-    <div className="ui-app-shell">
-      <HeaderRegion />
-      <GameWorldRegion />
-      <RightPanelHost />
-      <BottomBarRegion />
-    </div>
+    <IslandSelectionProvider>
+      <div className="ui-app-shell">
+        <HeaderRegion />
+        {isIsland ? <IslandWorldRegion /> : <GameWorldRegion />}
+        <RightPanelHost />
+        <BottomBarRegion />
+      </div>
+    </IslandSelectionProvider>
   );
 }
