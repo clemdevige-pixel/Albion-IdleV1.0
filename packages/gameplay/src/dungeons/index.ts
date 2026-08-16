c0ea73e48d73d3e38c01055504f1315231b1baa4
Kowzil <clem.devige@gmail.com>
2026-08-16 20:26:48 +0200

    feat: export dungeon runtime

---
+++ packages/gameplay/src/dungeons/index.ts
@@ -0,0 +1,12 @@
+export {
+  DungeonRuntime,
+  type DungeonAdvanceFailureReason,
+  type DungeonAdvanceResult,
+  type DungeonDefinition,
+  type DungeonEncounterDefinition,
+  type DungeonEncounterKind,
+  type DungeonRunState,
+  type DungeonRunStatus,
+  type DungeonStartFailureReason,
+  type DungeonStartResult,
+} from "./dungeon-runtime.js";
