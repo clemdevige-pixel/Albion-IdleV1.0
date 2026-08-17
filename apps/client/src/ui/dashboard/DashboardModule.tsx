a9c2b3fe831c9389a8721b07f2eb3243d0b9c284
Kowzil <clem.devige@gmail.com>
2026-08-17 08:10:32 +0200

    ui: bench player card from permanent dashboard

--- apps/client/src/ui/dashboard/DashboardModule.tsx
+++ apps/client/src/ui/dashboard/DashboardModule.tsx
@@ -1,18 +1,15 @@
 import "./dashboard.css";
 import {
-  useDashboardPlayer,
   useDashboardProduction,
   useDashboardYield,
   useDashboardZone,
   useDashboardZoneActions,
 } from "./useDashboardData";
 import { DashboardCombatCard } from "./components/DashboardCombatCard";
-import { DashboardPlayerCard } from "./components/DashboardPlayerCard";
 import { DashboardProductionCard } from "./components/DashboardProductionCard";
 import { DashboardYieldCard } from "./components/DashboardYieldCard";
 
 export function DashboardModule(): JSX.Element {
-  const player = useDashboardPlayer();
   const zone = useDashboardZone();
   const zoneActions = useDashboardZoneActions();
   const production = useDashboardProduction();
@@ -24,7 +21,6 @@ export function DashboardModule(): JSX.Element {
         zone={zone}
         onSetFarmMode={zoneActions.setFarmMode}
       />
-      <DashboardPlayerCard player={player} />
       <DashboardYieldCard yieldData={yieldData} />
       <DashboardProductionCard production={production} />
     </div>
