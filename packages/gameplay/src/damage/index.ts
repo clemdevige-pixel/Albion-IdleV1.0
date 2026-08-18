1c753a8eeeec001f2fdd68c9a789d45d2a3d48b6
Kowzil <clem.devige@gmail.com>
2026-08-18 17:09:26 +0200

    refactor(combat): export resistance curve helper

--- packages/gameplay/src/damage/index.ts
+++ packages/gameplay/src/damage/index.ts
@@ -2,7 +2,11 @@ export type { DamageType, DamageSourceType, DamageRequest, DamageResult } from "
 export type { HealthData } from "./components.js";
 export { HealthComponent } from "./components.js";
 export type { AttackerDamageStats, DefenderDamageStats, DamageCalculation } from "./damage-calculator.js";
-export { calculateDamage, RESISTANCE_CAP_PERCENT } from "./damage-calculator.js";
+export {
+  calculateDamage,
+  calculateResistanceMitigation,
+  RESISTANCE_SCALING_CONSTANT,
+} from "./damage-calculator.js";
 export { DamageValidator } from "./damage-validator.js";
 export { DamageManager } from "./damage-manager.js";
 export type { DamageContext } from "./damage-context.js";
