0f4e498419b899e6e4d4c1f39639b5538bf737db
Kowzil <clem.devige@gmail.com>
2026-08-19 14:23:09 +0200

    Add tiered dungeon completion silver rewards

--- apps/client/src/data/dungeonLootContentCatalog.ts
+++ apps/client/src/data/dungeonLootContentCatalog.ts
@@ -15,8 +15,17 @@ export interface DungeonLootDefinition {
   readonly faction: string;
   readonly artifactFragmentItemId: string;
   readonly artifactItemId: string;
+  readonly completionSilver: number;
   readonly encounters: Readonly<Record<DungeonEncounterKind, DungeonEncounterLootDefinition>>;
 }
+
+export const DUNGEON_COMPLETION_SILVER_BY_TIER = {
+  4: 2_500,
+  5: 5_000,
+  6: 10_000,
+  7: 20_000,
+  8: 40_000,
+} as const;
 
 const T4_FACTION_ENCOUNTER_LOOT: Readonly<Record<DungeonEncounterKind, DungeonEncounterLootDefinition>> = {
   normal: { artifactFragmentQuantity: 4, artifactDropChance: 0 }, elite: { artifactFragmentQuantity: 10, artifactDropChance: 0 }, boss: { artifactFragmentQuantity: 28, artifactDropChance: 0.1 },
@@ -34,30 +43,43 @@ const T8_FACTION_ENCOUNTER_LOOT: Readonly<Record<DungeonEncounterKind, DungeonEn
   normal: { artifactFragmentQuantity: 8, artifactDropChance: 0 }, elite: { artifactFragmentQuantity: 18, artifactDropChance: 0 }, boss: { artifactFragmentQuantity: 52, artifactDropChance: 0.18 },
 };
 
-function createFactionLootDefinition(id: string, faction: string, itemSuffix: string, encounters: Readonly<Record<DungeonEncounterKind, DungeonEncounterLootDefinition>>): DungeonLootDefinition {
-  return { id, faction, artifactFragmentItemId: `item_resource_artifact_fragment_${itemSuffix}`, artifactItemId: `item_resource_artifact_${itemSuffix}`, encounters };
+function createFactionLootDefinition(
+  id: string,
+  faction: string,
+  itemSuffix: string,
+  completionSilver: number,
+  encounters: Readonly<Record<DungeonEncounterKind, DungeonEncounterLootDefinition>>,
+): DungeonLootDefinition {
+  return {
+    id,
+    faction,
+    artifactFragmentItemId: `item_resource_artifact_fragment_${itemSuffix}`,
+    artifactItemId: `item_resource_artifact_${itemSuffix}`,
+    completionSilver,
+    encounters,
+  };
 }
 
-export const KEEPER_T4_DUNGEON_LOOT = createFactionLootDefinition(KEEPER_T4_LOOT_TABLE_ID, "Keeper", "keeper", T4_FACTION_ENCOUNTER_LOOT);
-export const HERETIC_T4_DUNGEON_LOOT = createFactionLootDefinition(HERETIC_T4_LOOT_TABLE_ID, "Heretic", "heretic", T4_FACTION_ENCOUNTER_LOOT);
-export const UNDEAD_T4_DUNGEON_LOOT = createFactionLootDefinition(UNDEAD_T4_LOOT_TABLE_ID, "Undead", "undead", T4_FACTION_ENCOUNTER_LOOT);
-export const MORGANA_T4_DUNGEON_LOOT = createFactionLootDefinition(MORGANA_T4_LOOT_TABLE_ID, "Morgana", "morgana", T4_FACTION_ENCOUNTER_LOOT);
-export const KEEPER_T5_DUNGEON_LOOT = createFactionLootDefinition(KEEPER_T5_LOOT_TABLE_ID, "Keeper", "keeper", T5_FACTION_ENCOUNTER_LOOT);
-export const HERETIC_T5_DUNGEON_LOOT = createFactionLootDefinition(HERETIC_T5_LOOT_TABLE_ID, "Heretic", "heretic", T5_FACTION_ENCOUNTER_LOOT);
-export const UNDEAD_T5_DUNGEON_LOOT = createFactionLootDefinition(UNDEAD_T5_LOOT_TABLE_ID, "Undead", "undead", T5_FACTION_ENCOUNTER_LOOT);
-export const MORGANA_T5_DUNGEON_LOOT = createFactionLootDefinition(MORGANA_T5_LOOT_TABLE_ID, "Morgana", "morgana", T5_FACTION_ENCOUNTER_LOOT);
-export const KEEPER_T6_DUNGEON_LOOT = createFactionLootDefinition(KEEPER_T6_LOOT_TABLE_ID, "Keeper", "keeper", T6_FACTION_ENCOUNTER_LOOT);
-export const HERETIC_T6_DUNGEON_LOOT = createFactionLootDefinition(HERETIC_T6_LOOT_TABLE_ID, "Heretic", "heretic", T6_FACTION_ENCOUNTER_LOOT);
-export const UNDEAD_T6_DUNGEON_LOOT = createFactionLootDefinition(UNDEAD_T6_LOOT_TABLE_ID, "Undead", "undead", T6_FACTION_ENCOUNTER_LOOT);
-export const MORGANA_T6_DUNGEON_LOOT = createFactionLootDefinition(MORGANA_T6_LOOT_TABLE_ID, "Morgana", "morgana", T6_FACTION_ENCOUNTER_LOOT);
-export const KEEPER_T7_DUNGEON_LOOT = createFactionLootDefinition(KEEPER_T7_LOOT_TABLE_ID, "Keeper", "keeper", T7_FACTION_ENCOUNTER_LOOT);
-export const HERETIC_T7_DUNGEON_LOOT = createFactionLootDefinition(HERETIC_T7_LOOT_TABLE_ID, "Heretic", "heretic", T7_FACTION_ENCOUNTER_LOOT);
-export const UNDEAD_T7_DUNGEON_LOOT = createFactionLootDefinition(UNDEAD_T7_LOOT_TABLE_ID, "Undead", "undead", T7_FACTION_ENCOUNTER_LOOT);
-export const MORGANA_T7_DUNGEON_LOOT = createFactionLootDefinition(MORGANA_T7_LOOT_TABLE_ID, "Morgana", "morgana", T7_FACTION_ENCOUNTER_LOOT);
-export const KEEPER_T8_DUNGEON_LOOT = createFactionLootDefinition(KEEPER_T8_LOOT_TABLE_ID, "Keeper", "keeper", T8_FACTION_ENCOUNTER_LOOT);
-export const HERETIC_T8_DUNGEON_LOOT = createFactionLootDefinition(HERETIC_T8_LOOT_TABLE_ID, "Heretic", "heretic", T8_FACTION_ENCOUNTER_LOOT);
-export const UNDEAD_T8_DUNGEON_LOOT = createFactionLootDefinition(UNDEAD_T8_LOOT_TABLE_ID, "Undead", "undead", T8_FACTION_ENCOUNTER_LOOT);
-export const MORGANA_T8_DUNGEON_LOOT = createFactionLootDefinition(MORGANA_T8_LOOT_TABLE_ID, "Morgana", "morgana", T8_FACTION_ENCOUNTER_LOOT);
+export const KEEPER_T4_DUNGEON_LOOT = createFactionLootDefinition(KEEPER_T4_LOOT_TABLE_ID, "Keeper", "keeper", DUNGEON_COMPLETION_SILVER_BY_TIER[4], T4_FACTION_ENCOUNTER_LOOT);
+export const HERETIC_T4_DUNGEON_LOOT = createFactionLootDefinition(HERETIC_T4_LOOT_TABLE_ID, "Heretic", "heretic", DUNGEON_COMPLETION_SILVER_BY_TIER[4], T4_FACTION_ENCOUNTER_LOOT);
+export const UNDEAD_T4_DUNGEON_LOOT = createFactionLootDefinition(UNDEAD_T4_LOOT_TABLE_ID, "Undead", "undead", DUNGEON_COMPLETION_SILVER_BY_TIER[4], T4_FACTION_ENCOUNTER_LOOT);
+export const MORGANA_T4_DUNGEON_LOOT = createFactionLootDefinition(MORGANA_T4_LOOT_TABLE_ID, "Morgana", "morgana", DUNGEON_COMPLETION_SILVER_BY_TIER[4], T4_FACTION_ENCOUNTER_LOOT);
+export const KEEPER_T5_DUNGEON_LOOT = createFactionLootDefinition(KEEPER_T5_LOOT_TABLE_ID, "Keeper", "keeper", DUNGEON_COMPLETION_SILVER_BY_TIER[5], T5_FACTION_ENCOUNTER_LOOT);
+export const HERETIC_T5_DUNGEON_LOOT = createFactionLootDefinition(HERETIC_T5_LOOT_TABLE_ID, "Heretic", "heretic", DUNGEON_COMPLETION_SILVER_BY_TIER[5], T5_FACTION_ENCOUNTER_LOOT);
+export const UNDEAD_T5_DUNGEON_LOOT = createFactionLootDefinition(UNDEAD_T5_LOOT_TABLE_ID, "Undead", "undead", DUNGEON_COMPLETION_SILVER_BY_TIER[5], T5_FACTION_ENCOUNTER_LOOT);
+export const MORGANA_T5_DUNGEON_LOOT = createFactionLootDefinition(MORGANA_T5_LOOT_TABLE_ID, "Morgana", "morgana", DUNGEON_COMPLETION_SILVER_BY_TIER[5], T5_FACTION_ENCOUNTER_LOOT);
+export const KEEPER_T6_DUNGEON_LOOT = createFactionLootDefinition(KEEPER_T6_LOOT_TABLE_ID, "Keeper", "keeper", DUNGEON_COMPLETION_SILVER_BY_TIER[6], T6_FACTION_ENCOUNTER_LOOT);
+export const HERETIC_T6_DUNGEON_LOOT = createFactionLootDefinition(HERETIC_T6_LOOT_TABLE_ID, "Heretic", "heretic", DUNGEON_COMPLETION_SILVER_BY_TIER[6], T6_FACTION_ENCOUNTER_LOOT);
+export const UNDEAD_T6_DUNGEON_LOOT = createFactionLootDefinition(UNDEAD_T6_LOOT_TABLE_ID, "Undead", "undead", DUNGEON_COMPLETION_SILVER_BY_TIER[6], T6_FACTION_ENCOUNTER_LOOT);
+export const MORGANA_T6_DUNGEON_LOOT = createFactionLootDefinition(MORGANA_T6_LOOT_TABLE_ID, "Morgana", "morgana", DUNGEON_COMPLETION_SILVER_BY_TIER[6], T6_FACTION_ENCOUNTER_LOOT);
+export const KEEPER_T7_DUNGEON_LOOT = createFactionLootDefinition(KEEPER_T7_LOOT_TABLE_ID, "Keeper", "keeper", DUNGEON_COMPLETION_SILVER_BY_TIER[7], T7_FACTION_ENCOUNTER_LOOT);
+export const HERETIC_T7_DUNGEON_LOOT = createFactionLootDefinition(HERETIC_T7_LOOT_TABLE_ID, "Heretic", "heretic", DUNGEON_COMPLETION_SILVER_BY_TIER[7], T7_FACTION_ENCOUNTER_LOOT);
+export const UNDEAD_T7_DUNGEON_LOOT = createFactionLootDefinition(UNDEAD_T7_LOOT_TABLE_ID, "Undead", "undead", DUNGEON_COMPLETION_SILVER_BY_TIER[7], T7_FACTION_ENCOUNTER_LOOT);
+export const MORGANA_T7_DUNGEON_LOOT = createFactionLootDefinition(MORGANA_T7_LOOT_TABLE_ID, "Morgana", "morgana", DUNGEON_COMPLETION_SILVER_BY_TIER[7], T7_FACTION_ENCOUNTER_LOOT);
+export const KEEPER_T8_DUNGEON_LOOT = createFactionLootDefinition(KEEPER_T8_LOOT_TABLE_ID, "Keeper", "keeper", DUNGEON_COMPLETION_SILVER_BY_TIER[8], T8_FACTION_ENCOUNTER_LOOT);
+export const HERETIC_T8_DUNGEON_LOOT = createFactionLootDefinition(HERETIC_T8_LOOT_TABLE_ID, "Heretic", "heretic", DUNGEON_COMPLETION_SILVER_BY_TIER[8], T8_FACTION_ENCOUNTER_LOOT);
+export const UNDEAD_T8_DUNGEON_LOOT = createFactionLootDefinition(UNDEAD_T8_LOOT_TABLE_ID, "Undead", "undead", DUNGEON_COMPLETION_SILVER_BY_TIER[8], T8_FACTION_ENCOUNTER_LOOT);
+export const MORGANA_T8_DUNGEON_LOOT = createFactionLootDefinition(MORGANA_T8_LOOT_TABLE_ID, "Morgana", "morgana", DUNGEON_COMPLETION_SILVER_BY_TIER[8], T8_FACTION_ENCOUNTER_LOOT);
 
 export const DUNGEON_LOOT_DEFINITIONS: Readonly<Record<string, DungeonLootDefinition>> = Object.fromEntries([
   KEEPER_T4_DUNGEON_LOOT, HERETIC_T4_DUNGEON_LOOT, UNDEAD_T4_DUNGEON_LOOT, MORGANA_T4_DUNGEON_LOOT,
