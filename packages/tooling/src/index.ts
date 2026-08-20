adcca43ea37c2ccee6ec0f3fd6560ef65d2f9b69
Kowzil <clem.devige@gmail.com>
2026-08-20 22:55:21 +0200

    chore: remove cancelled character rig exports

--- packages/tooling/src/index.ts
+++ packages/tooling/src/index.ts
@@ -9,23 +9,3 @@ export {
   ASSET_MANIFEST_RELATIVE,
   type AssetValidationSummary,
 } from "./validate-assets.js";
-export {
-  CHARACTER_RIG_ANIMATION_STATES,
-  CHARACTER_RIG_ANCHORS,
-  CHARACTER_RIG_HOLDING_PROFILES,
-  CHARACTER_RIG_ATTACK_PROFILES,
-  type CharacterRigAnimationState,
-  type CharacterRigAnchorId,
-  type CharacterRigHoldingProfile,
-  type CharacterRigAttackProfile,
-  type CharacterRigPoint,
-  type CharacterRigFrameAnchors,
-  type CharacterRigAnimationSource,
-  type CharacterRigLayerSource,
-  type CharacterRigWeaponProfile,
-  type CharacterRigManifest,
-} from "./character-rig/CharacterRigManifest.js";
-export {
-  CharacterRigValidationError,
-  validateCharacterRigManifest,
-} from "./character-rig/CharacterRigValidation.js";
