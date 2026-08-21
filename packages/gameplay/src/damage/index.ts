6247e551c58278ca5fdf717b5d3a7b4ed783a974
Kowzil <clem.devige@gmail.com>
2026-08-22 01:35:09 +0200

    feat(damage): export post-mitigation resolver type

--- packages/gameplay/src/damage/index.ts
+++ packages/gameplay/src/damage/index.ts
@@ -1,4 +1,10 @@
-export type { DamageType, DamageSourceType, DamageRequest, DamageResult } from "./types.js";
+export type {
+  DamageType,
+  DamageSourceType,
+  DamageRequest,
+  DamageResult,
+  PostMitigationDamageResolver,
+} from "./types.js";
 export type { HealthData } from "./components.js";
 export { HealthComponent } from "./components.js";
 export type { AttackerDamageStats, DefenderDamageStats, DamageCalculation } from "./damage-calculator.js";
