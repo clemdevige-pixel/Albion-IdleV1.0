e4ec374a2f106e9115de93f7f0b529731e59f28c
Kowzil <clem.devige@gmail.com>
2026-08-25 01:58:17 +0200

    client: remove faction rune world drop shim

--- apps/client/src/data/factionRuneWorldDropContentCatalog.ts
+++
@@ -1,9 +0,0 @@
-export {
-  FACTION_RUNE_WORLD_DROP_RATES,
-  getFactionRuneWorldDropChance,
-  getFactionRuneWorldDropExpectation,
-  isFactionRuneWorldDropEligibleFaction,
-  rollFactionRuneWorldDrop,
-  type FactionRuneWorldDrop,
-  type FactionRuneWorldDropZoneRate,
-} from "@game/data";
