b13914907bfa9683b14eceb833e1e3086a811fca
Kowzil <clem.devige@gmail.com>
2026-08-25 02:44:40 +0200

    data: author dungeon relic identity

---
+++ packages/data/src/config/dungeon-relic.ts
@@ -0,0 +1,12 @@
+export const DUNGEON_RELIC_ID = "relic_dungeon";
+export const DUNGEON_RELIC_ITEM_ID = "item_relic_dungeon";
+export const DUNGEON_RELIC_SOURCE_ZONE_ID = "zone_mountain_t4";
+export const DUNGEON_RELIC_SOURCE_SEGMENT_INDEX = 9;
+export const DUNGEON_RELIC_CHARGE_PER_FACTION = 50;
+
+export const DUNGEON_RELIC_REQUIRED_FACTIONS = [
+  "keeper",
+  "heretic",
+  "undead",
+  "morgana",
+] as const;
