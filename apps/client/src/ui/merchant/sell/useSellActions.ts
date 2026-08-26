9cbb73ed8da494a2239e3cd769af04f8bd478eb2
Kowzil <clem.devige@gmail.com>
2026-08-26 02:59:16 +0200

    refactor(client): source general vendor id from data

--- apps/client/src/ui/merchant/sell/useSellActions.ts
+++ apps/client/src/ui/merchant/sell/useSellActions.ts
@@ -1,3 +1,4 @@
+import { GENERAL_VENDOR_ID } from "@game/data";
 import { useVendorTransactionExecutor } from "../shared/useVendorTransactionExecutor";
 
 interface SellRequest {
@@ -10,6 +11,6 @@ interface SellRequest {
 export function useSellActions(): { readonly sell: (request: SellRequest) => boolean } {
   const execute = useVendorTransactionExecutor();
   return {
-    sell: (request) => execute({ direction: "sell", ...request }),
+    sell: (request) => execute({ vendorId: GENERAL_VENDOR_ID, direction: "sell", ...request }),
   };
 }
