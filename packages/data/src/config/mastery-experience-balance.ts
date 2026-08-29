ab2e99275f08d53485b5f09a51f2da10d746cf97
Kowzil <clem.devige@gmail.com>
2026-08-29 16:09:56 +0200

    balance: author weapon mastery endgame curve

--- packages/data/src/config/mastery-experience-balance.ts
+++ packages/data/src/config/mastery-experience-balance.ts
@@ -5,10 +5,25 @@ export interface MasteryExperienceCurveAuthoring {
   readonly maxLevel: number;
 }
 
-export const WEAPON_MASTERY_EXPERIENCE_BALANCE: MasteryExperienceCurveAuthoring = {
+export interface WeaponMasteryExperienceCurveAuthoring extends MasteryExperienceCurveAuthoring {
+  readonly generatedThroughLevel: number;
+  readonly authoredXpLevels66To100: readonly number[];
+}
+
+export const WEAPON_MASTERY_EXPERIENCE_BALANCE: WeaponMasteryExperienceCurveAuthoring = {
   baseXpLevels1To10: [100, 200, 300, 450, 650, 900, 1200, 1600, 2100, 2700],
   extensionCoefficient: 40,
   extensionExponent: 1.90,
+  generatedThroughLevel: 65,
+  authoredXpLevels66To100: [
+    104000, 107000, 111000, 114000, 118000,
+    121000, 125000, 130000, 133000, 137000,
+    140000, 145000, 149000, 152000, 157000,
+    198000, 204000, 209000, 215000, 221000,
+    226000, 231000, 237000, 243000, 249000,
+    255000, 260000, 266000, 272000, 278000,
+    284000, 290000, 297000, 303000, 309000,
+  ],
   maxLevel: 100,
 };
 
