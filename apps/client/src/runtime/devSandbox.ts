e0da98d4be150f8bbe9a4c3cfb67c6b2781e4891
Kowzil <clem.devige@gmail.com>
2026-08-29 19:40:15 +0200

    chore: reset dev sandbox for black market testing

--- apps/client/src/runtime/devSandbox.ts
+++ apps/client/src/runtime/devSandbox.ts
@@ -22,7 +22,7 @@ import {
   getDungeonKeyItemId,
 } from "../data/dungeonKeyContentCatalog.js";
 
-export const DEV_SANDBOX_SAVE_SLOT_ID = "albion_idle_dev_sandbox_v3";
+export const DEV_SANDBOX_SAVE_SLOT_ID = "albion_idle_dev_sandbox_v4";
 const DEV_SANDBOX_SILVER = 10_000_000;
 const DEV_SANDBOX_RESOURCE_STACK = 500;
 
